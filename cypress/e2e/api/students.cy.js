4c9c4a00d07b206fa9783f6474833e00faf52761
DRob97 <dylanlrobertson@gmail.com>
2024-03-02 14:01:41 -0600

    updated students.cy.js

--- cypress/e2e/api/students.cy.js
+++ cypress/e2e/api/students.cy.js
@@ -1,28 +1,15 @@
-// import {postRequestBody} from "../../fixtures/testData.json"
-
-// describe('CRUD Operations', () => {
-
-//     it('Create a new student using POST', () => {
-
-//         cy.request({
-//             method: 'POST',
-//             url: Cypress.env('baseUrl'),
-//             body: postRequestBody
-//         })
-
-//     })
-// })
-import { postRequestBody } from '../../fixtures/testData.json'
+import { postRequestBody, putRequestBody} from '../../fixtures/testData.json'
 
 describe('CRUD Operations', () => {
 
   // Temporary request to clean start.  
-  after(() => {
-    cy.request({
-      method: 'DELETE',
-      url: 'https://tech-global-training.com/students/deleteAll',
-    })
-  })
+  // after(() => {
+  //   cy.request({
+  //     method: 'DELETE',
+  //     url: 'https://tech-global-training.com/students/deleteAll',
+  //   })
+  // })
+  let userID
 
   it('Create a new student using POST', () => {
     cy.request({
@@ -32,6 +19,7 @@ describe('CRUD Operations', () => {
     }).then((response) => {
         // This is just a proof that we get the response of what we created
         cy.log(JSON.stringify(response.body))
+        userID = response.body.id
 
         // expect(response.status).to.equal(200)
         // expect(response.duration).to.be.below(200)
@@ -44,6 +32,47 @@ describe('CRUD Operations', () => {
         // })
 
         cy.validateResponse(response, postRequestBody)
+    })
+  })
+
+  it('Read the created student using GET', () => {
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('baseUrl')}/${userID}`
+    }).then((response) => {
+      expect(response.status).to.equal(200)
+      cy.log(JSON.stringify(response.body))
+    })
+  })
+
+  it('Update the student that we created using PUT', () => {
+    cy.request({
+      method: 'PUT',
+      url: `${Cypress.env('baseUrl')}/${userID}`,
+      body: putRequestBody
+    }).then((response) => {
+      expect(response.status).to.equal(200)
+      cy.validateResponse(response, putRequestBody)
+    })
+  })
+
+  it('Get the updated student using GET', () => {
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('baseUrl')}/${userID}`
+    }).then((response) => {
+      expect(response.status).to.equal(200)
+      expect(response.duration).to.be.below(400)
+      expect(response.body.firstName).to.equal(putRequestBody.firstName)
+    })
+  })
+
+  it('Delete the created/updated user', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `${Cypress.env('baseUrl')}/${userID}`
+    }).then((response) => {
+      expect(response.status).to.equal(200)
     })
   })
 })
